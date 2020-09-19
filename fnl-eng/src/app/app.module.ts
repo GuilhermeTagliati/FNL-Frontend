51b77bf25c6502964ec8c0d585b84a6913a75275
Guilherme Tagliati <guilhermetagliati@outlook.com>
2020-09-19 08:46:54 -0300

    Gestão de Assinantes V0.2.0

--- fnl-eng/src/app/app.module.ts
+++ fnl-eng/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    UserRegisterComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
